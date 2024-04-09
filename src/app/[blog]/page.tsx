5dc8c4d5200a7c8faef3681a44cd158c7bad2a56
mustafadurmaz <mdmz_001@hotmail.com>
2024-04-09 23:55:40 +0300

    not found component added

--- src/app/[blog]/page.tsx
+++ src/app/[blog]/page.tsx
@@ -1,13 +1,25 @@
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-interface Props {
-    params:any,
-}
+// interface Props {
+//     params:any,
+// }
 
-function Blog({params}: Props) {
+// function Blog({params}: Props) {
+//   return (
+//     <div>Blog:{params.blog} </div>
+//   )
+// }
+
+function Blog({searchParams}:any) {
+  if(searchParams.test==="true"){
+    notFound();
+  }
   return (
-    <div>Blog:{params.blog} </div>
+    <div>Blog </div>
   )
 }
+
+
 
 export default Blog
